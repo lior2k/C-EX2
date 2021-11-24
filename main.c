78b08d78b7dbd423a392e911f7adf29b529489dd
lior2k <92747945+lior2k@users.noreply.github.com>
2021-11-24 03:03:18 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -3,20 +3,27 @@
 
 int main() {
     char x;
-    int i,j;
+    int i,j,ans;
+    int arr[10][10];
     while (1) {
-        printf("enter a letter (A,B,C) \n");
+    //    printf("enter a letter (A,B,C) \n");
         scanf("%c", &x);
         if (x == 'A') {
-            func1();
+            func1(arr);
         } else if (x == 'B') {
-        //    printf("please enter i and j \n");
+    //        printf("please enter i and j \n");
             scanf("%d%d", &i, &j);
-            func2(i, j);
+            ans = func2(arr, i, j);
+            if (ans == 1) {
+                printf("True\n");
+            } else {
+                printf("False\n");
+            }
         } else if (x == 'C') {
-        //    printf("please enter i and j \n");
+    //        printf("please enter i and j \n");
             scanf("%d%d", &i, &j);
-            func3(i, j);
+            ans = func3(arr, i, j);
+            printf("%d\n", ans);
         } else if (x == 'D') {
             break;
         }
